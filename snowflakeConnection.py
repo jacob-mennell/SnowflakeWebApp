79847718c516486337ad2705aa6dc79be5cdf454
JacobMennell <jacobmennell@rockborne.com>
2022-04-08 12:07:46 +0100

    Added graph from JSON data

---
+++ snowflakeConnection.py
@@ -0,0 +1,20 @@
+#import relevant libs
+from snowflake import connector
+import os
+
+#define env variables
+password = os.getenv('password')
+username = os.getenv('username')
+
+def sfconnect():
+    cnx = connector.connect(
+        account='ia83659.europe-west2.gcp',
+        user=username,
+        password=password,
+        warehouse='COMPUTE_WH',
+        database='DEMO_DB',
+        schema='PUBLIC',
+        role='SYSADMIN')
+
+    return cnx
+
--- webserver.py
+++ webserver.py
@@ -1,12 +1,23 @@
+#import packages
 from flask import Flask, render_template, request
 from snowflake import connector
 import pandas as pd
 import os
+from snowflakeConnection import sfconnect
 
+#Flask Web Application
 app = Flask("my website")
 
 @app.route('/')
 def homepage():
+    cur = cnx.cursor().execute("SELECT COLOR_NAME, COUNT(*) "
+                               "FROM COLORS "
+                               "GROUP BY COLOR_NAME "
+                               "HAVING COUNT(*) > 50"
+                               "ORDER BY COUNT(*) DESC;")
+
+    rows = pd.DataFrame(cur.fetchall(), columns=['Color Name', 'Votes'])
+    dfhtml = rows.to_html(index=False)
     return render_template('index.html', dfhtml=dfhtml)
 
 @app.route('/submit')
@@ -17,32 +28,28 @@ def submitpage():
 def thanks4submit():
     colorname = request.form.get("cname")
     username = request.form.get("uname")
+
+    cnx.cursor().execute("INSERT INTO COLORS(COLOR_UID, COLOR_NAME) " +
+                         "SELECT COLOR_UID_SEQ.nextval, '" + colorname + "'")
     return render_template('thanks4submit.html',
                            colorname=colorname,
                            username=username)
+
+@app.route('/coolcharts')
+def coolcharts():
+    cur = cnx.cursor().execute("SELECT COLOR_NAME, COUNT(*) "
+                               "FROM COLORS "
+                               "GROUP BY COLOR_NAME ORDER BY COUNT(*) DESC;")
+    data4Charts = pd.DataFrame(cur.fetchall(), columns=['color', 'votes'])
+    data4ChartsJSON = data4Charts.to_json(orient='records')
+    return render_template('coolcharts.html', data4ChartsJSON=data4ChartsJSON)
+
+#Snowflake
+cnx = sfconnect()
 
 #define env variables
 password = os.getenv('password')
 username = os.getenv('username')
 
-#snowflake
-cnx = connector.connect(
-    account='ia83659.europe-west2.gcp',
-    user=username,
-    password=password,
-    warehouse='COMPUTE_WH',
-    database='DEMO_DB',
-    schema='PUBLIC',
-    role='SYSADMIN'
-)
-
-cur = cnx.cursor()
-cur.execute("SELECT * FROM COLORS")
-rows = pd.DataFrame(cur.fetchall(),columns=['COLOR_UID', 'COLOR_NAME'])
-#print(rows)
-
-#test dataframe as html
-dfhtml = rows.to_html()
-print(dfhtml)
 
 app.run()
